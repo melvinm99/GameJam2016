6d2d436aa036d43e75d09bd2ccdbd835d5da59f8
melvinm99 <massotti.melvin@gmail.com>
2016-01-30 11:25:35 +0100

    First Commit

---
+++ Assets/Scripts/Coll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+public class Coll {
+
+	public static int LEVA_CORRIDOIO=1;
+	public static int INDIZIO_MURO_CANTINA=2;
+	public static int COLTELLO_SALA_PRANZO=3;
+	public static int NOTE_CAMINO_SALA_PRANZO=4;
+	public static int OCCHI_STATUA_SALONE=5;
+
+
+}
